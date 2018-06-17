fe7e3edfed773640365e2a3f46f9211a3048ed00
Arthur Berkley <40262874+berkleywerkley@users.noreply.github.com>
2018-06-17 10:08:46 +0100

    Update Markov.py

--- Markov.py
+++ Markov.py
@@ -9,7 +9,8 @@ class Markov:
         self.prefix = ()
 
     def process_word(self, word, order =2):
-        
+        """Function to add relevant suffix to suffix_map depending on which prefix the word follows
+        order: number of words per prefix"""
         if len(self.prefix) < order:
             self.prefix += (word,)
             return
@@ -22,7 +23,7 @@ class Markov:
         self.prefix = self.shift(self.prefix, word)
 
     def process_file(self, filename, order = 2):
-
+        """performs process_word for each word in file"""
         fp = open(filename)
         self.skip_gutenberg_header(fp)
 
@@ -36,9 +37,12 @@ class Markov:
                 break
 
     def shift(self, t, word):
+        """moves focus along one word"""
         return t[1:] + (word,)
 
     def random_text(self, n=100):
+        """creates random text based on the prefix and suffix maps
+        n: length of text"""
         start = random.choice(list(self.suffix_map.keys()))
         
         for i in range(n):
@@ -51,8 +55,9 @@ class Markov:
             print(word, end=" ")
             start = self.shift(start,word)
 
-res = Markov()
-res.process_file("emma.txt",2) 
-res.random_text(100)
-print()           
+if __name__ == "__main__":
+    res = Markov()
+    res.process_file("emma.txt",2) 
+    res.random_text(100)
+    print()           
             
